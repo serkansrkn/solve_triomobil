c688bb9444efe2abb8ac1aae6121dc8e66f12b62
serkansrkn <55769261+serkansrkn@users.noreply.github.com>
2019-10-24 06:18:08 +0300

    Add project files.

---
+++ solve_triomobil/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace solve_triomobil
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.BtnEkle = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.BtnHesapla = new System.Windows.Forms.Button();
+            this.BtnTemizle = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(24, 26);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(130, 20);
+            this.numericUpDown1.TabIndex = 0;
+            this.numericUpDown1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // BtnEkle
+            // 
+            this.BtnEkle.Location = new System.Drawing.Point(24, 62);
+            this.BtnEkle.Name = "BtnEkle";
+            this.BtnEkle.Size = new System.Drawing.Size(130, 23);
+            this.BtnEkle.TabIndex = 1;
+            this.BtnEkle.Text = "Ekle";
+            this.BtnEkle.UseVisualStyleBackColor = true;
+            this.BtnEkle.Click += new System.EventHandler(this.BtnEkle_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(202, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 147);
+            this.listBox1.TabIndex = 2;
+            // 
+            // BtnHesapla
+            // 
+            this.BtnHesapla.Location = new System.Drawing.Point(24, 101);
+            this.BtnHesapla.Name = "BtnHesapla";
+            this.BtnHesapla.Size = new System.Drawing.Size(130, 23);
+            this.BtnHesapla.TabIndex = 3;
+            this.BtnHesapla.Text = "Hesapla";
+            this.BtnHesapla.UseVisualStyleBackColor = true;
+            this.BtnHesapla.Click += new System.EventHandler(this.BtnHesapla_Click);
+            // 
+            // BtnTemizle
+            // 
+            this.BtnTemizle.Location = new System.Drawing.Point(24, 141);
+            this.BtnTemizle.Name = "BtnTemizle";
+            this.BtnTemizle.Size = new System.Drawing.Size(130, 23);
+            this.BtnTemizle.TabIndex = 3;
+            this.BtnTemizle.Text = "Temizle";
+            this.BtnTemizle.UseVisualStyleBackColor = true;
+            this.BtnTemizle.Click += new System.EventHandler(this.BtnTemizle_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(71, 189);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(176, 31);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "En Büyük Toplam";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(104, 238);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 23);
+            this.label2.TabIndex = 6;
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.BtnEkle;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(334, 311);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BtnTemizle);
+            this.Controls.Add(this.BtnHesapla);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.BtnEkle);
+            this.Controls.Add(this.numericUpDown1);
+            this.Name = "Form1";
+            this.Text = "Trio Mobil";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button BtnEkle;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button BtnHesapla;
+        private System.Windows.Forms.Button BtnTemizle;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ solve_triomobil/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace solve_triomobil
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        List<int> liste = new List<int>();
+
+        int altKume = 0;
+        int sayi = 0;
+        int sayac = 0;
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            numericUpDown1.Minimum = -50000;
+            numericUpDown1.Maximum = 50000;
+        }
+
+        private void BtnEkle_Click(object sender, EventArgs e)
+        {
+            liste.Add((int)numericUpDown1.Value);
+            listBox1.Items.Add(numericUpDown1.Value);
+            numericUpDown1.Value = 0;
+        }
+
+        private void BtnHesapla_Click(object sender, EventArgs e)
+        {
+            int altKumeSayisi = (int)Math.Pow(2, liste.Count)/4;
+            int[] altKumeler = new int[altKumeSayisi];
+            int[] altKumeler2 = new int[altKumeSayisi];
+
+            for (int i = 0; i < altKumeSayisi; i++)
+            {
+                sayi = i;
+                altKume = 0;
+                sayac = 0;
+
+                for (int j = 0; j < liste.Count; j+=2)
+                {
+                    if (sayi % 2 == 1)
+                    {
+                        if (sayac == 0){ altKume += liste[j]; }
+                        else { altKume += liste[j]; }
+                        sayac++;
+                    }
+
+                    if (sayi == 1) { break; }
+                    if (sayi >= 2) { sayi /= 2; }
+                }
+                altKumeler[i] = altKume;
+            }
+
+            for (int i = 0; i < altKumeSayisi; i++)
+            {
+                sayi = i;
+                altKume = 0;
+                sayac = 0;
+
+                for (int j = 1; j < liste.Count; j += 2)
+                {
+                    if (sayi % 2 == 1)
+                    {
+                        if (sayac == 0) { altKume += liste[j]; }
+                        else { altKume += liste[j]; }
+                        sayac++;
+                    }
+
+                    if (sayi == 1) { break; }
+                    if (sayi >= 2) { sayi /= 2; }
+                }
+                altKumeler2[i] = altKume;
+            }
+
+            int mak = altKumeler[0];
+            for (int i = 1; i < altKumeSayisi; i++)
+            {     
+                if (mak < altKumeler[i])
+                    mak = altKumeler[i];
+            }
+
+            int mak2 = altKumeler2[0];
+            for (int i = 1; i < altKumeSayisi; i++)
+            {
+                if (mak2 < altKumeler2[i])
+                    mak2 = altKumeler2[i];
+            }
+
+            if(mak>mak2)
+            {
+                label2.Text = mak.ToString();
+            }
+            else
+            {
+                label2.Text = mak2.ToString();
+            }
+        }
+
+        private void BtnTemizle_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            liste.Clear();
+            altKume = 0;
+        }
+
+    }
+}
